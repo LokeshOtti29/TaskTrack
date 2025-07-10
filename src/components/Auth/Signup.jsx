36634b7788d9f1c8cb5837cf1b501c6585a316b0
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-10 19:04:06 +0530

    login-signup-page-created

---
+++ src/components/Auth/Signup.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-200 relative">
+      <div className="absolute inset-0 z-50 flex items-center justify-center ">
+        <div className="bg-white text-black p-4 rounded shadow w-[800px] h-[500px] flex flex-row">
+          <div
+            className="flex-1 bg-cover bg-center"
+            style={{ backgroundImage: "url('/signup.jpg')" }}
+          ></div>
+          <div className="flex-1 flex items-center justify-center"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
