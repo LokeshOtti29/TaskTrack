4bfb08a3135d28891849d61305383883cd00241f
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-12 18:50:01 +0530

    created-form

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,89 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(20, "Username Should be less than 20 characters"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+  accept: z.boolean().optional(),
+});
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log("form submitted", data);
+  };
   return (
-    <div className="w-full min-h-screen bg-gray-200 relative">
+    <div className="w-full min-h-screen bg-[rgb(246,130,122)] relative">
       <div className="absolute inset-0 z-50 flex items-center justify-center ">
         <div className="bg-white text-black p-4 rounded shadow w-[800px] h-[500px] flex flex-row">
-          <div className="flex-1 flex items-center justify-center"></div>
-
-          <div
-            className="flex-1 bg-cover bg-center"
-            style={{ backgroundImage: "url('/login.png')" }}
-          ></div>
+          <div className="flex-1 flex items-start justify-start px-10 py-10">
+            <div className="flex flex-col my-3 w-full ">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-3"
+              >
+                <div className="flex-1 py-4 ">
+                  <span className="text-xl font-semibold ">Sign In</span>
+                </div>
+                <div className="flex-2">
+                  <input
+                    type="text"
+                    placeholder="username"
+                    {...register("name")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.name && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.name.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-2">
+                  <input
+                    type="password"
+                    placeholder="password"
+                    {...register("password")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.password && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.password.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-1 flex flex-row items-center gap-2">
+                  <input type="checkbox" {...register("accept")} />
+                  <p>Remember Me</p>
+                </div>
+                <div className="flex-1">
+                  <button
+                    type="submit"
+                    className="w-3/5 bg-[rgb(246,130,122)] text-white py-2 rounded border hover:border-black"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="flex-1 flex items-end justify-center">
+            <div
+              className="w-full h-[350px] bg-contain bg-start bg-no-repeat"
+              style={{ backgroundImage: "url('/login.png')" }}
+            ></div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,15 +1,142 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  firstname: z
+    .string()
+    .min(1, "firstname is required")
+    .max(20, "firstname Should be less than 20 characters"),
+  lastname: z
+    .string()
+    .min(1, "lastname is required")
+    .max(20, "lastname Should be less than 20 characters"),
+  username: z
+    .string()
+    .min(1, "username is required")
+    .max(20, "username Should be less than 20 characters"),
+  email: z.string().min(1, "Email is required").email("Enter a valid email"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+  terms: z.literal(true, {
+    errorMap: () => ({ message: "You must accept the terms and conditions" }),
+  }),
+});
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log("form submitted", data);
+  };
   return (
-    <div className="w-full min-h-screen bg-gray-200 relative">
+    <div className="w-full min-h-screen bg-[rgb(246,130,122)] relative">
       <div className="absolute inset-0 z-50 flex items-center justify-center ">
         <div className="bg-white text-black p-4 rounded shadow w-[800px] h-[500px] flex flex-row">
-          <div
-            className="flex-1 bg-cover bg-center"
-            style={{ backgroundImage: "url('/signup.jpg')" }}
-          ></div>
-          <div className="flex-1 flex items-center justify-center"></div>
+          <div className="flex-1 flex items-end justify-center">
+            <div
+              className="w-full h-[400px] bg-contain bg-start bg-no-repeat"
+              style={{ backgroundImage: "url('/signup.jpg')" }}
+            ></div>
+          </div>
+          <div className="flex-1 flex items-center justify-center">
+            <div className="flex flex-col  w-full ">
+              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+                <div className="flex-1 py-4 ">
+                  <span className="text-xl font-semibold ">Sign Up</span>
+                </div>
+                <div className="flex-2">
+                  <input
+                    type="text"
+                    placeholder="Enter FirstName"
+                    {...register("firstname")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.firstname && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.firstname.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-2">
+                  <input
+                    type="text"
+                    placeholder="Enter LastName"
+                    {...register("lastname")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.lastname && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.lastname.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-2">
+                  <input
+                    type="text"
+                    placeholder="Enter UserName"
+                    {...register("username")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.username && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.username.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <input
+                    type="email"
+                    placeholder="Enter Email"
+                    {...register("password")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.password && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.password.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <input
+                    type="password"
+                    placeholder="Enter Password"
+                    {...register("password")}
+                    className="w-full px-3 py-2 border rounded"
+                  />
+                  <div className="h-5">
+                    {errors.password && (
+                      <p className="text-red-600 text-sm leading-tight">
+                        {errors.password.message}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex-1 flex flex-row items-center gap-2 py-3">
+                  <input type="checkbox" {...register("terms")} />
+                  <p>I agree to all terms</p>
+                </div>
+                <div className="flex-1">
+                  <button
+                    type="submit"
+                    className="w-3/5 bg-[rgb(246,130,122)] text-white py-2 rounded border hover:border-black"
+                  >
+                    Register
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
