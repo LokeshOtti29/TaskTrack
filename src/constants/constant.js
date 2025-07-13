8d9caae97c130f2bc33c892e973e52d865500389
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-13 18:54:28 +0530

    adding-constants

---
+++ src/constants/constant.js
@@ -0,0 +1,14 @@
+export const SIDE_BAR = [
+  {
+    IMAGE: "/Dashboard.png",
+    NAME: "Dashboard",
+  },
+  {
+    IMAGE: "/Task.png",
+    NAME: "My Task",
+  },
+  {
+    IMAGE: "/Setting.png",
+    NAME: "Settings",
+  },
+];
