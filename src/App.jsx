38afab2c6689b314ed2982d2f85f87fd7222a487
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-23 01:09:56 +0530

    Changes-in-app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Login from "./components/Auth/Login";
 import Home from "./components/UI/Home";
+import Rendercomponent from "./components/UI/Rendercomponent";
 
 function App() {
   return (
     <>
-      <Home />
+      <Rendercomponent />
     </>
   );
 }
--- src/components/UI/Home.jsx
+++ src/components/UI/Home.jsx
@@ -2,10 +2,9 @@ import React, { useRef, useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Rendercomponent from "./Rendercomponent";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
-  const [page, setPage] = useState(0);
-
   return (
     <div className="h-screen flex flex-col">
       <header>
@@ -14,10 +13,10 @@ const Home = () => {
 
       <main className="flex flex-1 flex-row gap-2">
         <div className="basis-1/6 h-full">
-          <Sidebar setPage={setPage} />
+          <Sidebar />
         </div>
         <div className="basis-4/6  ml-12 mt-8 mb-8 flex flex-col items-start pl-8 pt-4 pr-8 border shadow-lg rounded-lg">
-          <Rendercomponent page={page} />
+          <Outlet />
         </div>
       </main>
     </div>
